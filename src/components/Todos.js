598be2e156ac55d2efae9a30a50cab3fba4aa089
Meghana Rajappa <52892025+meghanar121@users.noreply.github.com>
2020-03-07 12:22:04 +0100

    Add files via upload

---
+++ src/components/Todos.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Todoitems from './Todoitems';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render(){
+   return this.props.todos.map((todo) => (
+   <Todoitems key={todo.id} todo={todo}markComplete={this.props.markComplete}
+   delTodo={this.props.delTodo}/>
+   ));
+}
+}
+
+//propTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
+export default Todos;
