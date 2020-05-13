13416fa840b54ec97c38378cd6badebdb17890fc
meghanar121 <meghanar121@gmail.com>
2020-05-13 11:27:08 +0200

    Updates

---
+++ precache-manifest.657bfe643b586cd334b04d8780cf13b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559c0451e0762e063afc0a2bd88a624e",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f01ea1acf1062bead366",
+    "url": "/react-todo/static/css/main.4e556bf3.chunk.css"
+  },
+  {
+    "revision": "d16ff4969b184e7d5c16",
+    "url": "/react-todo/static/js/2.420bd3e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.420bd3e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f01ea1acf1062bead366",
+    "url": "/react-todo/static/js/main.77ce271c.chunk.js"
+  },
+  {
+    "revision": "cecda34d35e0b2b06330",
+    "url": "/react-todo/static/js/runtime-main.33271c6e.js"
+  }
+]);
